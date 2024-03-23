300f90480b9eb7edd156a9c7e6963f3a8335348e
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2024-03-23 10:48:48 +0530

    Created first URLs and Views and configure them

---
+++ 01_Basic/myweb/challenges/urls.py
@@ -0,0 +1,8 @@
+# To create urls we create urlpatterns list and values inside the list is path()-> to specific view
+from django.urls import path
+# importing from same folder following syntax is used
+from . import views
+
+urlpatterns = [
+    path("january",views.index)
+]
--- 01_Basic/myweb/challenges/views.py
+++ 01_Basic/myweb/challenges/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Creating first function based view
+
+
+def index(request):
+
+    return HttpResponse("Hurray!! This is working.")
