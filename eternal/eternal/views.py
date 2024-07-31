022e6142c111db73f1f584d6668956e2ba07f0c1
Mohd Gulam Ansari <gulamansari57181@gmail.com>
2024-07-31 14:10:17 +0530

    Implemented views and render html file successfully

--- eternal/eternal/views.py
+++ eternal/eternal/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+# To render html file
+
+from django.shortcuts import render
 
 def home(request):
     
-    return HttpResponse("<h1>Welcome to Chai's Django Project: Home page</h1>")
+    # return HttpResponse("<h1>Welcome to Chai's Django Project: Home page</h1>")
+    return render(request, 'websites/index.html')
 
 
 def about(request):
